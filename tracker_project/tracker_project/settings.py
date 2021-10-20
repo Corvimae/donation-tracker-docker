16687b4b71cf22dd0d3f57058d7abf52e8753f45
May Roussel <may@maybreak.com>
2021-10-20 13:30:03 -0500

    read domain from env

--- tracker_project/tracker_project/settings.py
+++ tracker_project/tracker_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['teamvelocitysr.com']
+ALLOWED_HOSTS = [os.getenv('DOMAIN')]
 
 # Application definition
 
@@ -144,7 +144,7 @@ from tracker import ajax_lookup_channels
 AJAX_LOOKUP_CHANNELS = ajax_lookup_channels.AJAX_LOOKUP_CHANNELS
 ASGI_APPLICATION = 'tracker_project.routing.application'
 CHANNEL_LAYERS = {'default': {'BACKEND': 'channels.layers.InMemoryChannelLayer'}}
-DOMAIN = 'teamvelocitysr.com'
+DOMAIN = `os.getenv('DOMAIN')`
 
 HAS_EMAIL = True
 EMAIL_USE_TLS = True
